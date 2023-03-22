47b2591a74574cbc15d011b364ad123af4ce765d
HeAmazigh <amazighhettal@gmail.com>
2023-03-22 18:47:14 +0100

    Add address

--- components/info/Info.component.tsx
+++ components/info/Info.component.tsx
@@ -8,11 +8,11 @@ export const Info = () => {
       <div className='flex-1 mb-5'>
         <p className='flex justify-center items-center text-sm md:text-lg lg:text-1xl text-[#0080FE]'>CONTACT</p>
         <span className='flex justify-center gap-3 text-center'> <AiOutlineMail color='#0080FE' size='20' /> transaylan@yahoo.com</span>
-        <span className='flex justify-center gap-3 text-center'> <AiOutlinePhone color='#0080FE' size='20' />  0778 644 567</span>
+        <span className='flex justify-center gap-3 text-center'> <AiOutlinePhone color='#0080FE' size='20' /> 0778 644 567</span>
       </div>
       <div className='flex-1 mb-5'>
         <p className='flex justify-center items-center text-sm md:text-lg lg:text-1xl text-[#0080FE]'>ADRESSE</p>
-        <span className='flex justify-center gap-3 text-center'> <CiLocationOn color='#0080FE' size='20' /> 1234 rue adresse paris</span>
+        <span className='flex justify-center gap-3 text-center'> <CiLocationOn color='#0080FE' size='20' /> 06 rue du Maréchal Foch, 94310 Orly</span>
       </div>
       <div className='flex-1 mb-5'>
         <p className='flex justify-center items-center text-sm md:text-lg lg:text-1xl text-[#0080FE]'>HORAIRES D'OUVERTURE</p>
