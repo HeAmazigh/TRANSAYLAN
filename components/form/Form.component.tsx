29545d050546a758c421cf9acc86d47fe2ad70a4
HeAmazigh <amazighhettal@gmail.com>
2023-04-02 11:58:50 +0100

    Added email service

--- components/form/Form.component.tsx
+++ components/form/Form.component.tsx
@@ -1,26 +1,121 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import emailjs from '@emailjs/browser';
+import Spinner from 'react-bootstrap/Spinner';
+
+import { useToasts } from 'react-toast-notifications';
 
 export const Form = () => {
+  const [send, setSend] = useState(false);
+  const formContact = useRef();
+  const { addToast } = useToasts();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setSend(true);
+    emailjs.sendForm(
+        'service_66yp60m', 
+        'template_cd5olwg', 
+        formContact.current, 
+        'nXniB3oDz0HCAonDi'
+      )
+      .then((result) => {
+        if(result.status == 200) {
+          addToast('Demande envoyé avec succès !', { appearance: 'success' });
+          e.target.reset();
+          setSend(false);
+        };
+
+      }, (error) => {
+        addToast('Erreur d\'envoi ', { appearance: 'error' });
+        setSend(false);
+      });
+  };
+
   return (
     <div id='devis' className='w-full px-1 -mt-36 md:px-20 lg:px-48 z-50'>
-      <div className='w-full bg-white m-auto shadow-2xl rounded-lg p-1 xs:p-4 md:p-4'>
+      <div className='w-full bg-white m-auto shadow-2xl rounded-lg p-1 xs:p-4 ≠md:p-4'>
         <div className='text-center text-lg md:text-3xl text-[#0080FE] font-bold'>Obtenir un devis gratuit</div>
-        <form className='flex flex-col gap-5 mt-6'>
+        <form ref={formContact} onSubmit={sendEmail} className="flex flex-col gap-5 mt-6">
           <div className='flex flex-col md:flex-row items-center justify-between gap-3 px-2 md:px-20'>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="text" placeholder='Nom / Prénom'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="email" placeholder='E-mail'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="phone" placeholder='Téléphone'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="date" placeholder='Date du déménagement'/>
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' 
+              type="text" 
+              placeholder='Nom / Prénom'
+              name="fullname" 
+              required
+            />
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md'
+              type="email" 
+              placeholder='E-mail'
+              name="email" 
+              required
+            />
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' 
+              type="phone" 
+              placeholder='Téléphone'
+              name="phone" 
+              required
+            />
+            <input
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' 
+              type="date" 
+              placeholder='Date du déménagement'
+              name="date" 
+              required
+            />
           </div>
           <div className='flex flex-col md:flex-row items-center justify-between gap-3 px-2 md:px-20'>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="text" placeholder='Ville de départ'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="text" placeholder='Code postal ville de départ'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="text" placeholder='Ville d’arrivée'/>
-            <input className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' type="text" placeholder='ode postal ville d’arrivée'/>
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md'
+              type="text"
+              placeholder='Ville de départ'
+              name="dcity" 
+              required
+            />
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md'
+              type="text" 
+              placeholder='Code postal ville de départ'
+              name="dpc" 
+              required
+            />
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' 
+              type="text" 
+              placeholder='Ville d’arrivée'
+              name="acity" 
+              required
+            />
+            <input 
+              className='w-full border-2 border-slate-100 text-xs h-7 px-4 rounded-md' 
+              type="text" 
+              placeholder='Code postal ville d’arrivée'
+              name="apc" 
+              required
+            />
           </div>
-          <textarea className='mx-2 md:mx-20 border-2 border-slate-100 text-xs h-14 px-4 rounded-md' placeholder='Message'></textarea>
+          <textarea 
+            className='mx-2 md:mx-20 border-2 border-slate-100 text-xs h-14 px-4 rounded-md' 
+            placeholder='Message'
+            name="message" 
+            required
+          >
+
+          </textarea>
           <div className='text-center'>
-            <button className='bg-[#0080FE] text-white py-2 px-2 md:px-4 rounded-sm hover:bg-[#0080FE] transition-colors ease-linear'>Envoyer</button>
+            <button  className='bg-[#0080FE] text-white py-2 px-2 md:px-4 rounded-sm hover:bg-[#0080FE] transition-colors ease-linear' disabled={send}>
+            {send && <Spinner
+              className='mr-2'
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            /> }
+              Envoyer
+            </button>
           </div>
         </form>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastProvider } from 'react-toast-notifications';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ToastProvider> <Component {...pageProps} /> </ToastProvider>
 }
